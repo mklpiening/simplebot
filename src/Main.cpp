2d16a09549c2a9afe6a703de07405ab119873d5c
Malte kl. Piening <mkl.piening@gmail.com>
2019-07-27 01:44:09 +0200

    fixed indentation

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,54 +13,54 @@ Motor* motors[] = {&a0, &a1, &b0, &b1};
 MotorSynchronizer sync(4, motors);
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 }
 
 void loop() {
-  a0.readPulsesFromSensor();
-  a1.readPulsesFromSensor();
-  b0.readPulsesFromSensor();
-  b1.readPulsesFromSensor();
+    a0.readPulsesFromSensor();
+    a1.readPulsesFromSensor();
+    b0.readPulsesFromSensor();
+    b1.readPulsesFromSensor();
 
-  if(sync.synchronize()) {
-    Serial.println();
-    Serial.print("A0 ");
-    Serial.print(a0.getPulses());
-    Serial.print(" ");
-    Serial.println(a0.getSpeedMultiplicator());
-    Serial.print("A1 ");
-    Serial.print(a1.getPulses());
-    Serial.print(" ");
-    Serial.println(a1.getSpeedMultiplicator());
-    Serial.print("B0 ");
-    Serial.print(b0.getPulses());
-    Serial.print(" ");
-    Serial.println(b0.getSpeedMultiplicator());
-    Serial.print("B1 ");
-    Serial.print(b1.getPulses());
-    Serial.print(" ");
-    Serial.println(b1.getSpeedMultiplicator());
+    if(sync.synchronize()) {
+        Serial.println();
+        Serial.print("A0 ");
+        Serial.print(a0.getPulses());
+        Serial.print(" ");
+        Serial.println(a0.getSpeedMultiplicator());
+        Serial.print("A1 ");
+        Serial.print(a1.getPulses());
+        Serial.print(" ");
+        Serial.println(a1.getSpeedMultiplicator());
+        Serial.print("B0 ");
+        Serial.print(b0.getPulses());
+        Serial.print(" ");
+        Serial.println(b0.getSpeedMultiplicator());
+        Serial.print("B1 ");
+        Serial.print(b1.getPulses());
+        Serial.print(" ");
+        Serial.println(b1.getSpeedMultiplicator());
 
-    a0.resetPulses();
-    a1.resetPulses();
-    b0.resetPulses();
-    b1.resetPulses();
-  }
-
-  
-
-  if (Serial.read() == 0xff) {
-    delay(2);
-    uint8_t dirs = Serial.read();
-    if ((dirs & 0b11111100) == 0) {
-      int16_t speedL = (((dirs & 0b00000010) >> 1) == 1 ? -1 : 1) * Serial.read();
-      int16_t speedR = ((dirs  & 0b00000001) == 1 ? -1 : 1) * Serial.read();
-      
-      a0.setSpeed(speedR);
-      b0.setSpeed(speedR);
-
-      a1.setSpeed(speedL);
-      b1.setSpeed(speedL);
+        a0.resetPulses();
+        a1.resetPulses();
+        b0.resetPulses();
+        b1.resetPulses();
     }
-  }
+
+    
+
+    if (Serial.read() == 0xff) {
+        delay(2);
+        uint8_t dirs = Serial.read();
+        if ((dirs & 0b11111100) == 0) {
+        int16_t speedL = (((dirs & 0b00000010) >> 1) == 1 ? -1 : 1) * Serial.read();
+        int16_t speedR = ((dirs  & 0b00000001) == 1 ? -1 : 1) * Serial.read();
+        
+        a0.setSpeed(speedR);
+        b0.setSpeed(speedR);
+
+        a1.setSpeed(speedL);
+        b1.setSpeed(speedL);
+        }
+    }
 }
